dd95261b2da0b8d6a5224db75bdc5bf86466d9af
AhCamargo <andre.camargo500@gmail.com>
2025-04-10 15:34:47 -0300

    Added new rules of bran and oil table

--- src/templates/contrato.tsx
+++ src/templates/contrato.tsx
@@ -57,7 +57,7 @@ const ContratoTemplate: React.FC<ContratoTemplateProps> = ({
     : "";
 
   // Só iremos remover essa regra das siglas, caso o cliente aceite a sugestão da reunião do dia 09/04/2025
-  const listProducts = ["O", "F", "OC", "OA", "SB", "EP"];
+  const listProducts = ["O", "OC", "OA", "SB", "EP"];
   const validProducts = listProducts.includes(formData.product);
   const siglaProduct = validProducts ? "O" : formData.product;
 
